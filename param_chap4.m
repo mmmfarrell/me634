8e97a83501c0d20f0052b2502c0e5702e7ce623d
Michael <michaeldavidfarrell@gmail.com>
2017-02-03 10:23:21 -0700

    I think this works right

--- param_chap4.m
+++ param_chap4.m
@@ -12,10 +12,10 @@ P.gravity = 9.8;
 P.lambda = 100;
 
 % initial conditions
-P.pn0    = 0;  % initial North position
+P.pn0    = -400;  % initial North position
 P.pe0    = 0; % initial East position
 P.pd0    = 0;  % initial Down position (negative altitude)
-P.u0     = 0;  % initial velocity along body x-axis
+P.u0     = 25;  % initial velocity along body x-axis
 P.v0     = 0;  % initial velocity along body y-axis
 P.w0     = 0;  % initial velocity along body z-axis
 P.phi0   = 0;  % initial roll angle
@@ -30,9 +30,9 @@ P.Va0 = 25;
 P.Ts = 0.01;
 
 % Wind
-P.wind_n    = 5;
-P.wind_e    = 5;
-P.wind_d    = 5;
+P.wind_n    = 0;
+P.wind_e    = 0;
+P.wind_d    = 0;
 
 % Dryden Gust Model Parameters
 % Low alt, light turb
